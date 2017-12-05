37cc314efa59a66d1710d4d0398319e7ac00c474
KingBack18434303450 <King18434393450@163.com>
2017-12-05 12:02:15 +0800

    diyici

---
+++ 01第一个php程序.php
@@ -0,0 +1,45 @@
+<html>
+	
+	<head>
+		<title>
+			这是一个网站
+		</title>
+		
+		<meta charset="utf-8" />
+		
+		
+		<style>
+			
+		div{
+			
+			color: red;
+			
+		}
+			
+			
+			
+		</style>
+		
+		
+	</head>
+	
+	<body>
+		
+	
+	<div>
+		这是一个php程序
+		
+	</div>
+	  
+	  <script>
+	  	
+	  	 document.write("js代码");
+	  	
+	  	
+	  	
+	  </script>
+		
+		
+	</body>
+	
+</html>
